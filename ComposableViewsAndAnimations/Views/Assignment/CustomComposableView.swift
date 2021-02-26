c4b10d56a695517ae2e1886d1a786f314b152e36
Bradley Kang <bkang@lcs.on.ca>
2021-02-26 12:29:49 -0500

    Started creating the array for the encouraging words

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct CustomComposableView: View {
+    
+    // MARK: Stored Properties
+    
+    @State var message: String = ""
+    
+    // MARK: Computed Properties
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(message)
+            .font(Font.custom("", size: 20.0, relativeTo: .largeTitle))
+            .onTapGesture {
+
+            }
     }
+    
+    // MARK: Functions
+
 }
 
 struct CustomComposableView_Previews: PreviewProvider {
---
+++ ComposableViewsAndAnimations/Views/Assignment/EncouragingWords.swift
@@ -0,0 +1,11 @@
+//
+//  EncouragingWords.swift
+//  ComposableViewsAndAnimations
+//
+//  Created by Brad Kang on 2021-02-26.
+//
+
+import Foundation
+
+let encouragingWords = [
+    "Good job!", "Keep going!", ]
