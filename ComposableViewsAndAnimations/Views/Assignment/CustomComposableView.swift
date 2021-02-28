004995644c542c104de2eeb4a62bc3f0916e19e4
Bradley Kang <bkang@lcs.on.ca>
2021-02-28 12:11:26 -0500

    Added onAppear and onTapGesture and some of the animations and changes that will happen when it appears

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -13,15 +13,30 @@ struct CustomComposableView: View {
     
     @State var message: String = ""
     
-    var hue: Color = .red
+    @State var fontSize: Double = 25.0
+    
+    @State var hue: Color = .red
     
     // MARK: Computed Properties
     
+    private static let fontNames: [String] = {
+        var names: [String] = []
+        for familyName in UIFont.familyNames {
+            names.append(contentsOf: UIFont.fontNames(forFamilyName: familyName))
+        }
+        return names.sorted()
+    }()
+    
     var body: some View {
         Text(message)
-            .font(Font.custom("", size: 20.0, relativeTo: .largeTitle))
+            .foregroundColor(hue)
+            .font(Font.custom(message, size: CGFloat(fontSize)))
+            .onAppear() {
+                message = encouragingWords[Int.random(in: 0...5)]
+            }
             .onTapGesture {
-                
+                fontSize = Double.random(in: 15...50)
+                hue = Color(hue: Double.random(in: 1...360) / 360, saturation: 0.8, brightness: 0.8)
             }
     }
     
