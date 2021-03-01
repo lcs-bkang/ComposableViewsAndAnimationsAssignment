c36ec670c39a4dd5651f295e2456da9bcdd4a23c
Bradley Kang <bkang@lcs.on.ca>
2021-03-01 11:58:18 -0500

    Abstracted out the text and put it into an overlay over the rectangle

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -21,7 +21,7 @@ struct CustomComposableView: View {
     @State var hue: Double = 60.0
     
     // Controls offset
-    @State var offset: CGFloat = 500.0
+    @State var offset: CGFloat = 0
     
     // MARK: Computed Properties
     
@@ -31,26 +31,16 @@ struct CustomComposableView: View {
     
     var body: some View {
         
-        ZStack {
+        VStack {
             
-            Text(message)
-                .foregroundColor(Color(hue: hue, saturation: 0.8, brightness: 0.8))
-                .font(.custom(message, size: CGFloat(fontSize)))
-                .offset(x: 0, y: offset)
-                .onAppear() {
-                    message = encouragingWords[Int.random(in: 0...5)]
-                    fontSize = Double.random(in: 30...50)
-                    hue = Double.random(in: 1...360) / 360
-                    offset -= 500
-            }
-                .animation(.interpolatingSpring(stiffness: 7, damping: 2, initialVelocity: 6))
-            
+
             Rectangle()
                 .frame(width: 150.0, height: nil)
                 .foregroundColor(Color(hue: complementaryHue.normalized, saturation: 0.8, brightness: 0.8))
                 .padding(.vertical, 225.0)
                 .rotationEffect(.degrees(90))
                 .offset(x: 3, y: offset)
+                .overlay(TextOverlay())
                 
         }
     }
---
+++ ComposableViewsAndAnimations/Views/Assignment/TextOverlay.swift
@@ -0,0 +1,46 @@
+//
+//  TextOverlay.swift
+//  ComposableViewsAndAnimations
+//
+//  Created by Brad Kang on 2021-03-01.
+//
+
+import SwiftUI
+
+struct TextOverlay: View {
+    
+    // The message that will be shown
+    @State var message: String = ""
+    
+    // The size of the font
+    @State var fontSize: Double = 25.0
+    
+    // Color of the text
+    @State var hue: Double = 60.0
+    
+    // Controls offset
+    @State var offset: CGFloat = 500.0
+    
+    var body: some View {
+        
+        ZStack {
+        Text(message)
+            .foregroundColor(Color(hue: hue, saturation: 0.8, brightness: 0.8))
+            .font(.custom(message, size: CGFloat(fontSize)))
+            .offset(x: 0, y: offset)
+            .onAppear() {
+                message = encouragingWords[Int.random(in: 0...5)]
+                fontSize = Double.random(in: 30...50)
+                hue = Double.random(in: 1...360) / 360
+                offset -= 500
+        }
+            .animation(.interpolatingSpring(stiffness: 7, damping: 2, initialVelocity: 6))
+        }
+    }
+}
+
+struct TextOverlay_Previews: PreviewProvider {
+    static var previews: some View {
+        TextOverlay()
+    }
+}
